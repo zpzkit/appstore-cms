f8c5005483643c9634965c55e347364937b6cedd
zhangpeng <peng.zhang@pintuibao.cn>
2017-08-30 17:43:50 +0800

    add appstore cms

---
+++ src/main/java/com/mobvoi/appstore/cms/controller/operation/ListApp.java
@@ -0,0 +1,13 @@
+package com.mobvoi.appstore.cms.controller.operation;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by zhang (pengzhang@mobvoi.com)on 2017/8/30.
+ */
+@RestController
+@RequestMapping("appstore/cms/list")
+public class ListApp {
+
+}
